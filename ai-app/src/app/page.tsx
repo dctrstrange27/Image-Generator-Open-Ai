038192c23f19f0f6076ea89d14b40e870ddc4ada
dctrrohan@gmail.com <dctrrohan@gmail.com>
2023-08-05 12:29:45 +0800

    add:short description on landing page

--- ai-app/src/app/page.tsx
+++ ai-app/src/app/page.tsx
@@ -8,6 +8,13 @@ export default function Home() {
           Unleash the Power of DALL-E AI
         </h1>
         <h2 className="text-2xl">Enter a World of Visually Stunning Imagery</h2>
+        <br></br>
+        <p className="font-inter max-w-4xl">
+          Embrace the revolution of AI-generated art and witness the incredible
+          possibilities it holds. Whether you're an artist seeking new
+          dimensions or an art lover craving fresh perspectives, DALL-E Artistry
+          will redefine how you perceive the world.
+        </p>
       </div>
     </main>
   );
